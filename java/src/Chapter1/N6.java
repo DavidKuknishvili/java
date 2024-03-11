91e0c9c90808016e1bfd20c360a5c30e77ce871e
David Kuknishvili <92639690+DavidKuknishvili@users.noreply.github.com>
2024-03-11 23:54:21 +0400

    Add files via upload

---
+++ java/src/Chapter1/N6.java
@@ -0,0 +1,13 @@
+package Chapter1;
+
+public class N6 {
+    public static void main(String[] args) {
+
+        int x = 0;
+        for (int i = 1; i <=9 ; i++) {
+            x += i;
+        }
+        System.out.println(x);
+
+    }
+}
